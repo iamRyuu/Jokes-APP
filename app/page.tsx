6c5a3d88d473d684e812718b4e2a70d762539542
Jorge Obando <ryuuzaki.dev@gmail.com>
2024-08-30 20:08:11 -0600

    change styles for get a static header

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
 import JokeGenerator from "@/components/JokeGenerator";
 import { ModeToggle } from "@/components/mode-toggle";
-import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center py-2">
-      <ModeToggle />
-      <JokeGenerator />
+    <main className="flex min-h-screen flex-col">
+      <header className="w-full fixed top-0 left-0 p-4 flex justify-end">
+        <ModeToggle />
+      </header>
+
+      <div className="flex-grow flex items-center justify-center mt-16">
+        <JokeGenerator />
+      </div>
     </main>
   );
 }
